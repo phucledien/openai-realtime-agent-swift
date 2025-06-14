78405c3cf6a80f8789c4215bb131e1a07dc5ead0
phucledien <phucledien@gmail.com>
2025-06-14 17:47:09 +0700

    Initial Commit

---
+++ RealtimeAgent/RealtimeAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeAgentApp.swift
+//  RealtimeAgent
+//
+//  Created by Oliver Le on 14/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
